a78814fc5b2e83db0af36392cb6f06ab8757ee8d
Qasim Wani <qasim31wani@gmail.com>
2020-03-18 04:40:34 -0400

    added update file. convert to batch and use it on Raspberry Pi for continous uptime management. if no problems, then use HitchHiqe's resources.'

---
+++ Diagnostics/get_updates.py
@@ -0,0 +1,67 @@
+### Assumption : Confirmed_cases.csv exists in the working directory
+from selenium import webdriver
+import pandas as pd
+import os
+import re
+from selenium.webdriver import ActionChains
+from selenium.common.exceptions import NoSuchElementException  
+from selenium.webdriver.common.keys import Keys
+import time
+import numpy as np
+import random
+from datetime import datetime
+import collections
+
+def init():
+    URL = "https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.headless = True
+    prefs = {"profile.default_content_setting_values.notifications" : 2}
+    chrome_options.add_experimental_option("prefs",prefs)
+    br = webdriver.Chrome(executable_path=r"C:\Users/qasim/Downloads/chromedriver_win32_v80/chromedriver.exe",options=chrome_options)
+    br.get(URL)
+    time.sleep(3)
+    return br
+
+if __name__ == "__main__":
+
+    root = init().find_element_by_id("ember6")
+
+    data = root.text.split("\n")[2:]
+
+    end = data.index("Last Updated at (M/D/YYYY)")
+
+    total_cases = data[1]
+
+    start = 1 + data.index("Confirmed Cases by Country/Region/Sovereignty")
+
+    data = data[start:end]
+
+    total_cases = int(total_cases.replace(",",""))
+
+    TIME = str(datetime.now())
+
+    case = {}
+
+    for x in data:
+        x = x.split(" ")
+        country = " ".join(x[1:]).replace("*", "")
+        cases = int(x[0].replace(",",""))
+        case[country] = cases
+
+    case = collections.OrderedDict(sorted(case.items()))
+
+    df = pd.read_csv("Confirmed_case.csv")
+
+    df_temp = pd.DataFrame.from_dict(case, orient="index", columns=[':'.join(TIME.split(":")[:-1])]).reset_index()
+
+    del df_temp["index"]
+
+    df_cum = [df, df_temp]
+
+    df_cum = pd.concat(df_cum, axis=1)
+
+    df_cum.to_csv("Confirmed_case.csv", index=False)
+    
+    print("Successfully updated Cofirmed_case.csv file")
